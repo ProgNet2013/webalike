72ded5ec3e1497e0d3054e988b5cf58dac175b73
Dylan Beattie <dylan@dylanbeattie.net>
2013-05-29 16:10:15 +0100

    Added ReadyRoll database package
    Verified that Visual Studio/msbuild DO create a .nupkg containing the
    database migration

---
+++ src/Webalike.Database/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("Webalike.Database_Title")]
+[assembly: AssemblyDescription("Webalike.Database_Description")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Webalike.Database_Company")]
+[assembly: AssemblyProduct("Webalike.Database_Product")]
+[assembly: AssemblyCopyright("Webalike.Database_Copyright")]
+[assembly: AssemblyTrademark("Webalike.Database_Trademark")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyFileVersion("5.6.7.8")]
